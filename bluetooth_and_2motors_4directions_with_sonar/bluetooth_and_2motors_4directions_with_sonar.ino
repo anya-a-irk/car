b0c40a3926d2c0a75c3f9df746149c1c8b8135c0
Anna Abdrakhimova <anya.a.irk@gmail.com>
2016-04-09 14:45:45 +0300

    fixing project

--- bluetooth_and_2motors_4directions_with_sonar/bluetooth_and_2motors_4directions_with_sonar.ino
+++ bluetooth_and_2motors_4directions_with_sonar/bluetooth_and_2motors_4directions_with_sonar.ino
@@ -4,16 +4,15 @@
 #include <Ultrasonic.h>
 Ultrasonic ultrasonic(22,23);
 
-#include <AFMotor.h>  // Подключаем библиотеку для работы с шилдом
-
+#include <AFMotor.h>
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
-
+int power=255;
 char symbol;
 
 void setup() {
   Serial.begin(9600);
-  Serial3.begin(9600);   // инициализация порта
+  Serial3.begin(9600);
   Serial3.println("Enter symbol");
   Serial3.println("'0' - RELEASE");
   Serial3.println("'1' - FORWARD");
@@ -34,9 +33,9 @@ void loop() {
     delay(500);
 
     motor1.run(BACKWARD);
-    motor1.setSpeed(255);
+    motor1.setSpeed(power);
     motor2.run(BACKWARD);
-    motor2.setSpeed(255);
+    motor2.setSpeed(power);
     delay(500);
 
     motor1.run(RELEASE);
@@ -44,9 +43,9 @@ void loop() {
     delay(500);
 
     motor1.run(RELEASE);
-    motor1.setSpeed(255);
+    motor1.setSpeed(power);
     motor2.run(FORWARD);
-    motor2.setSpeed(255);
+    motor2.setSpeed(power);
     delay(1000);
   }
    if (Serial3.available() > 0) {
@@ -57,28 +56,28 @@ void loop() {
      delay(500);
      if (symbol == '1') {
        motor1.run(FORWARD);
-       motor1.setSpeed(255);
+       motor1.setSpeed(power);
        motor2.run(FORWARD);
-       motor2.setSpeed(255);
+       motor2.setSpeed(power);
      }
      if (symbol == '2') {
        motor1.run(BACKWARD);
-       motor1.setSpeed(255);
+       motor1.setSpeed(power);
        motor2.run(BACKWARD);
-       motor2.setSpeed(255);
+       motor2.setSpeed(power);
      }
      if (symbol == '3') {
        motor1.run(RELEASE);
-       motor1.setSpeed(255);
+       motor1.setSpeed(power);
        motor2.run(FORWARD);
-       motor2.setSpeed(255);
+       motor2.setSpeed(power);
      }
 
      if (symbol == '4') {
        motor1.run(FORWARD);
-       motor1.setSpeed(255);
+       motor1.setSpeed(power);
        motor2.run(RELEASE);
-       motor2.setSpeed(255);
+       motor2.setSpeed(power);
      }
      if (symbol == '0') {
        motor1.run(RELEASE);
